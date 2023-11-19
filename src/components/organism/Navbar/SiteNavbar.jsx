943245ffbdb7fe44ab1bcb19ccf5493c3324b06e
rakanugraha18 <rakanugraha2@gmail.com>
2023-11-19 22:23:25 +0700

    React test 6

--- src/components/organism/Navbar/SiteNavbar.jsx
+++ src/components/organism/Navbar/SiteNavbar.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import Icon from '../../Atoms/Icon';
-import Logo from '../../Atoms/Logo';
-import List from '../../Atoms/Global/ListItem';
-import SearchButton from '../../molecule/Search/SearchButton';
-import ThemeSwitch from '../../molecule/ThemeSwitch';
+import React, { useState } from "react";
+import Icon from "../../Atoms/Icon";
+import Logo from "../../Atoms/Logo";
+import List from "../../Atoms/Global/List";
+import SearchButton from "../../molecule/Search/SearchButton";
+import ThemeSwitch from "../../molecule/ThemeSwitch";
 
 const SiteNavbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -14,20 +14,36 @@ const SiteNavbar = () => {
 
   return (
     <nav className="relative h-full w-full flex mx-auto items-center justify-between">
-      <ul id='nav-link' className="relative flex w-full h-full items-center">
+      <ul id="nav-link" className="relative flex w-full h-full items-center">
         <Logo />
-        <div className='only-medium'>
-        <List redirect="/store" text="Store" className="h-full px-[8px] lg:px-[16px]" title="Store" />
-        <List
-          redirect="/smartphone"
-          text="SmartPhone"
-          className="h-full px-[8px] lg:px-[16px]"
-          title="SmartPhone"
-        /></div>
+        <div className="only-medium">
+          <List
+            redirect="/store"
+            text="Store"
+            className="h-full px-[8px] lg:px-[16px]"
+            title="Store"
+          />
+          <List
+            redirect="/smartphone"
+            text="SmartPhone"
+            className="h-full px-[8px] lg:px-[16px]"
+            title="SmartPhone"
+          />
+        </div>
         <div className="grow h-full"></div>
-        <div className='only-medium'>
-        <List redirect="#" text="Discover" className="h-full px-[8px] lg:px-[16px]" title="Discover" />
-        <List redirect="#" text="Support" className="h-full px-[8px] lg:px-[16px]" title="Support" />
+        <div className="only-medium">
+          <List
+            redirect="#"
+            text="Discover"
+            className="h-full px-[8px] lg:px-[16px]"
+            title="Discover"
+          />
+          <List
+            redirect="#"
+            text="Support"
+            className="h-full px-[8px] lg:px-[16px]"
+            title="Support"
+          />
         </div>
         <SearchButton />
         <Icon redirect="/cart" classname="shopping-cart" />
@@ -49,22 +65,50 @@ const SiteNavbar = () => {
           <div className="only-phone fixed top-0 left-0 w-screen bg-background h-screen z-[80] py-10">
             <div className="relative max-w-full w-full z-[1001] flex flex-col">
               {/* Your new list items */}
-              <div className='text-text relative mt-10'>
-                <div className='px-[16px]  py-[16px]  flex' >
-                  <div className='w-max rounded-lg bg-neutral-300 text-white flex items-center justify-center'>
-                <Icon redirect="/account" classname="account" /></div>
-                <List className="mr-10 inline-block text-text px-[8px] lg:px-[16px]" redirect="/account" text="Account" title="Login/Register" />
+              <div className="text-text relative mt-10">
+                <div className="px-[16px]  py-[16px]  flex">
+                  <div className="w-max rounded-lg bg-neutral-300 text-white flex items-center justify-center">
+                    <Icon redirect="/account" classname="account" />
+                  </div>
+                  <List
+                    className="mr-10 inline-block text-text px-[8px] lg:px-[16px]"
+                    redirect="/account"
+                    text="Account"
+                    title="Login/Register"
+                  />
                 </div>
-                <div className='py-5 border-b-gray-100 border-t-gray-100 border-b-[1px] border-t-[1px]'>
-                <List className="text-text px-[8px] lg:px-[16px]" redirect="/store" text="Store" title="Store" />
-                <List className="text-text px-[8px] lg:px-[16px]" redirect="/smartphone" text="Smartphone" title="Smartphone" />
-                <List redirect="#" text="Discover" className="px-[8px] lg:px-[16px]" title="Discover" />
-                <List redirect="#" text="Support" className="px-[8px] lg:px-[16px]" title="Support" />
+                <div className="py-5 border-b-gray-100 border-t-gray-100 border-b-[1px] border-t-[1px]">
+                  <List
+                    className="text-text px-[8px] lg:px-[16px]"
+                    redirect="/store"
+                    text="Store"
+                    title="Store"
+                  />
+                  <List
+                    className="text-text px-[8px] lg:px-[16px]"
+                    redirect="/smartphone"
+                    text="Smartphone"
+                    title="Smartphone"
+                  />
+                  <List
+                    redirect="#"
+                    text="Discover"
+                    className="px-[8px] lg:px-[16px]"
+                    title="Discover"
+                  />
+                  <List
+                    redirect="#"
+                    text="Support"
+                    className="px-[8px] lg:px-[16px]"
+                    title="Support"
+                  />
                 </div>
-                <List className="text-text p-2" redirect="/about" text="About" title="About" />
-               
- 
-
+                <List
+                  className="text-text p-2"
+                  redirect="/about"
+                  text="About"
+                  title="About"
+                />
               </div>
               {/* Add more list items as needed */}
             </div>
@@ -75,4 +119,4 @@ const SiteNavbar = () => {
   );
 };
 
-export default SiteNavbar; 
+export default SiteNavbar;
